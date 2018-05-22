91e2195f70d11e455dc59ed6057818ee83506910
Eric Poels <epoels@ucsc.edu>
2018-05-21 17:27:37 -0700

    Initial Schema for Slugtutor

--- models/tables.py
+++ models/tables.py
@@ -30,5 +30,53 @@ db.checklist.updated_on.writable = db.checklist.updated_on.readable = False
 db.checklist.id.writable = db.checklist.id.readable = False
 
 
+db.define_table('appointments',
+	           Field('date', 'datetime'),
+	           Field('tutor'),
+	           Field('can_reschedule', 'boolean'),
+	           Field('student'),
+	           Field('rate', 'double', default=11.00)
+	           primarykey = ['date', 'tutor']
+	           )
+
+db.appointments.tutor.writable = False
+db.appointments.tutor.readable = False
+db.appointments.can_reschedule.writable = False
+db.appointments.can_reschedule.readable = False
+db.appointments.id.writable = db.appointments.id.readable = False
+
+
+# Things that should be done about this table
+# Populate it with all classes that are on the registery
+# Create a 'add_class function where someone can add a class to the database' 
+db.define_table('classes',
+	            Field('title'),
+	            Field('number', 'integer'),
+	            Field('professor'),
+	            Field('department'),
+	            primarykey = ['name', 'number', 'department'],
+	            )
+
+db.define_table('student',
+	            Field('name'),
+	            Field('email', default=get_user_email()),
+	            Field('major'),
+	            Field('year'),
+                primarykey = ['name', 'email', 'year']
+	            )
+
+db.student.email.readable = False
+db.student.email.writable = False
+
+db.define_table('tutor',
+                Field('name'),
+                Field('email'),
+                Field('major'),
+                Field('year'),
+                Field('classes'),
+                Field('rating', 'double', default=0.0),
+                primarykey = ['name', 'email', 'major']
+	            )
+
 # after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
