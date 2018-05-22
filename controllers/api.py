f8a7dfd5e15631af94b3275c31339dbebed3bb93
nmeddin <nicholas.a.meddin@gmail.com>
2018-05-21 20:27:49 -0700

    added an almost working random quote thing

--- controllers/api.py
+++ controllers/api.py
@@ -1,1 +1,7 @@
-# Here go your api methods.
+# These code snippets use an open-source library. http://unirest.io/python
+response = unirest.get("https://healthruwords.p.mashape.com/v1/quotes/?id=731&maxR=1&size=medium&t=Wisdom",
+  headers={
+    "X-Mashape-Key": "NN569DEEiWmshBASQyyNkfCW1ybEp1Saa5Djsndgm5A62xkHEY",
+    "Accept": "application/json"
+  }
+)
