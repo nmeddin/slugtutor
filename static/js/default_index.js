4efb3c17dbf6ab873678ea60f3fc3ab679baeef0
Colin Maher <colinmaher123@gmail.com>
2018-05-24 16:47:27 -0700

    fix conflicts

--- static/js/default_index.js
+++ static/js/default_index.js
@@ -13,20 +13,70 @@ var app = function() {
         }
     };
 
-    self.toggle_pub = function() {
-        self.vue.is_public = !self.vue.is_public;
-        console.log(self.vue.is_public);
-    };
+    // Some classes.
+    var CMPS109 = {id: 1, title: 'Advanced Programming', 'demand': 0};
+    var CMPS121 = {id: 2, title: 'Mobile Applications', 'demand': 0};
+    var CMPS183 = {id: 3, title: 'Web Applications', 'demand': 0};
+    var class_list = [CMPS109, CMPS121, CMPS183];
+
+    // Compenent testing.
+
+    Vue.component('child', {
+      props: ['cla'],
+      template: '<p>{{ cla.title }}</p>'
+        
+    });
+	 
+	 
+	 Vue.component('my-checkbox', {
+		template: '#checkbox-template',
+		data() {
+		  return {
+			 checked: false,
+			 title: 'Check me'
+		  }
+		},
+		methods: {
+		  check() {
+			 this.checked = !this.checked;
+		  }
+		}
+	 });
+    
+    Vue.component('class-demand', {
+        data: function () {
+            return {
+                count: 0
+            }
+        },
+        props: ['title'],
+        template: 
+            `<div align = "justify">{{ title }}: {{count}}</div>`
+    })
+
+    // Vue.component('class_demand,' {
+    //     props: {
+    //         propA: String,
+    //         propB: Number,
+
+    //     },
+    //     template: '<p>${ title }: ${ demand }</p>'
+    // });
+
     // Complete as needed.
     self.vue = new Vue({
         el: "#vue-div",
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
-        data: {
-            is_pub: false
+        data: function () {
+            return {
+            message: 'hello world',
+            class_list: class_list,
+				selected: ''
+            }
         },
         methods: {
-            toggle_pub: self.toggle_pub
+//            increment()
         }
 
     });
